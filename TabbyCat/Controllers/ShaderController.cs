e0040d57228b519a29a99a3cc9e9dd294e471b12
John Kerr <john@jmkerr.com>
2020-03-07 14:40:04 +0000

    Update ShaderController.cs

--- TabbyCat/Controllers/ShaderController.cs
+++ TabbyCat/Controllers/ShaderController.cs
@@ -9,7 +9,6 @@
         internal ShaderController(PropertyController propertyController)
         {
             PropertyController = propertyController;
-            Editor.btnExportHTML.Click += BtnExportHTML_Click;
         }
 
         private void BtnExportHTML_Click(object sender, System.EventArgs e)
@@ -34,12 +33,61 @@
         {
             if (connect)
             {
-
+                Editor.btnDocumentMap.Click += DocumentMap_Click;
+                Editor.btnExportHTML.Click += BtnExportHTML_Click;
+                Editor.btnExportRTF.Click += ExportRTF_Click;
+                Editor.btnHelp.Click += Help_Click;
+                Editor.btnLineNumbers.Click += LineNumbers_Click;
+                Editor.btnPrint.Click += Print_Click;
+                Editor.btnRuler.Click += Ruler_Click;
+                Editor.btnSplit.Click += Split_Click;
             }
             else
             {
-
+                Editor.btnDocumentMap.Click -= DocumentMap_Click;
+                Editor.btnExportHTML.Click -= BtnExportHTML_Click;
+                Editor.btnExportRTF.Click -= ExportRTF_Click;
+                Editor.btnHelp.Click -= Help_Click;
+                Editor.btnLineNumbers.Click -= LineNumbers_Click;
+                Editor.btnPrint.Click -= Print_Click;
+                Editor.btnRuler.Click -= Ruler_Click;
+                Editor.btnSplit.Click -= Split_Click;
             }
+        }
+
+        private void Split_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Ruler_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Print_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void LineNumbers_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Help_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ExportRTF_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DocumentMap_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
