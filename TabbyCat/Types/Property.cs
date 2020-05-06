2a5ecc27695467819c8c8021a13a509d6c3d6a0d
John Kerr <john@jmkerr.com>
2020-05-06 15:15:50 +0100

    Add Property enum.

---
+++ TabbyCat/Types/Property.cs
@@ -0,0 +1,55 @@
+﻿namespace TabbyCat.Types
+{
+    internal enum Property
+    {
+        None,
+
+        // Scene properties
+
+        Background,
+        Camera,
+        CameraFocus,
+        CameraPosition,
+        FarPlane,
+        FieldOfView,
+        TargetFPS,
+        GLTargetVersion,
+        NearPlane,
+        ProjectionType,
+        Samples,
+        SceneTitle,
+        Signals,
+        Stereo,
+        Traces,
+        VSync,
+
+        // Signal properties
+
+        SignalName,
+        SignalAmplitude,
+        SignalAmplitudeMaximum,
+        SignalAmplitudeMinimum,
+        SignalFrequency,
+        SignalFrequencyMaximum,
+        SignalFrequencyMinimum,
+        SignalWaveType,
+
+        // Trace properties
+
+        TraceDescription,
+        TraceLocation,
+        TraceMaximum,
+        TraceMinimum,
+        TraceOrientation,
+        TracePattern,
+        TraceScale,
+        TraceStripeCount,
+        TraceVisible,
+
+        // GPU properties
+
+        GPULog,
+        GPUStatus,
+        GraphicsMode
+    }
+}
