9b602e6258560dbde2316ffe6bd30311d1c445bc
John Kerr <john@jmkerr.com>
2020-05-11 00:19:56 +0100

    Update DockingCon.cs

--- TabbyCat/Controllers/DockingCon.cs
+++ TabbyCat/Controllers/DockingCon.cs
@@ -4,9 +4,15 @@
 
     public abstract class DockingCon : LocalizationCon
     {
+        // Constructors
+
         protected DockingCon(WorldCon worldCon) : base(worldCon) { }
 
+        // Protected properties
+
         protected abstract DockContent Form { get; }
+
+        // Public methods
 
         public void SetVisibility(bool visible)
         {
@@ -15,6 +21,8 @@
             else
                 Form.Hide();
         }
+
+        // Protected methods
 
         protected void ToggleVisibility() => SetVisibility(!Form.Visible);
     }
