f3e08acaca08d0846b57ebb7a1796a7b320ce246
John Kerr <john@jmkerr.com>
2020-05-11 00:27:14 +0100

    Update HotkeysCon.cs

--- TabbyCat/Controllers/HotkeysCon.cs
+++ TabbyCat/Controllers/HotkeysCon.cs
@@ -12,26 +12,26 @@
 
         // Private fields
 
-        private HotkeysForm _HotkeysForm;
+        private HotkeysForm _hotkeysForm;
 
         // Protected properties
 
         protected override DockContent Form => HotkeysForm;
 
-        protected override HotkeysForm HotkeysForm => _HotkeysForm ?? (_HotkeysForm = new HotkeysForm());
+        protected override HotkeysForm HotkeysForm => _hotkeysForm ?? (_hotkeysForm = new HotkeysForm());
+
+        // Public methods
 
         public override void Connect(bool connect)
         {
             base.Connect(connect);
             if (connect)
-            {
                 WorldForm.HelpCodeHotkeys.Click += HelpCodeEditorKeys_Click;
-            }
             else
-            {
                 WorldForm.HelpCodeHotkeys.Click -= HelpCodeEditorKeys_Click;
-            }
         }
+
+        // Private Methods
 
         private void HelpCodeEditorKeys_Click(object sender, EventArgs e) => ToggleVisibility();
     }
