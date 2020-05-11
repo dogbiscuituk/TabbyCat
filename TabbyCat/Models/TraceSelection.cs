a0ac06404534dafdf28d354afc819902932857a4
John Kerr <john@jmkerr.com>
2020-05-11 12:43:07 +0100

    Update TraceSelection.cs

--- TabbyCat/Models/TraceSelection.cs
+++ TabbyCat/Models/TraceSelection.cs
@@ -7,67 +7,37 @@
     using System.Linq;
     using Types;
 
-    public partial class TraceSelection : Selection<Trace>
+    public class TraceSelection : Selection<Trace>, IScript
     {
         // Public properties
 
-        public string Description
-        {
-            get => GetProperty(p => p.Description);
-            set => SetProperty(p => p.Description = value);
-        }
+        public string Description => GetProperty(p => p.Description);
 
-        public Vector3 Location
-        {
-            get => GetVector3(p => p.Location);
-            set => SetProperty(p => p.Location = value);
-        }
+        public Vector3 Location => GetVector3(p => p.Location);
 
-        public Vector3 Maximum
-        {
-            get => GetVector3(p => p.Maximum);
-            set => SetProperty(p => p.Maximum = value);
-        }
+        public Vector3 Maximum => GetVector3(p => p.Maximum);
 
-        public Vector3 Minimum
-        {
-            get => GetVector3(p => p.Minimum);
-            set => SetProperty(p => p.Minimum = value);
-        }
+        public Vector3 Minimum => GetVector3(p => p.Minimum);
 
-        public Vector3 Orientation
-        {
-            get => GetVector3(p => p.Orientation);
-            set => SetProperty(p => p.Orientation = value);
-        }
+        public Vector3 Orientation => GetVector3(p => p.Orientation);
 
-        public Pattern Pattern
-        {
-            get => (Pattern)GetProperty(p => (int)p.Pattern);
-            set => SetProperty(p => p.Pattern = value);
-        }
+        public Pattern Pattern => (Pattern)GetProperty(p => (int)p.Pattern);
 
-        public Vector3 Scale
-        {
-            get => GetVector3(p => p.Scale);
-            set => SetProperty(p => p.Scale = value);
-        }
+        public Vector3 Scale => GetVector3(p => p.Scale);
 
-        public Vector3 StripeCount
-        {
-            get => GetVector3(p => p.StripeCount);
-            set => SetProperty(p => p.StripeCount = value);
-        }
-
-        public bool? Visible
-        {
-            get => GetBool(p => p.Visible);
-            set => SetProperty(p => p.Visible = value == true);
-        }
+        public Vector3 StripeCount => GetVector3(p => p.StripeCount);
 
         public IEnumerable<Trace> Traces => Items.OrderBy(p => p.Index);
 
+        public bool? Visible => GetBool(p => p.Visible);
+
+        // Public methods
+
+        public string GetScript(ShaderType shaderType) => GetProperty(p => p.GetScript(shaderType)) ?? string.Empty;
+
         public IEnumerable<int> GetTraceIndices() => Items.Select(p => p.Index);
+
+        public void SetScript(ShaderType shaderType, string value) => SetProperty(p => p.SetScript(shaderType, value));
 
         // Private methods
 
@@ -75,12 +45,5 @@
             GetProperty(p => f(p).X),
             GetProperty(p => f(p).Y),
             GetProperty(p => f(p).Z));
-    }
-
-    public partial class TraceSelection : IScript
-    {
-        public string GetScript(ShaderType shaderType) => GetProperty(p => p.GetScript(shaderType)) ?? string.Empty;
-
-        public void SetScript(ShaderType shaderType, string value) => SetProperty(p => p.SetScript(shaderType, value));
     }
 }
