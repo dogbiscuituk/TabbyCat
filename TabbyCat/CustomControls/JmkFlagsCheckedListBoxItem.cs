9872d67cbee6143db630729c0ca0304ed5d68368
John Kerr <john@jmkerr.com>
2020-05-11 11:41:28 +0100

    Update JmkFlagsCheckedListBoxItem.cs

--- TabbyCat/CustomControls/JmkFlagsCheckedListBoxItem.cs
+++ TabbyCat/CustomControls/JmkFlagsCheckedListBoxItem.cs
@@ -8,20 +8,9 @@
             Value = value;
         }
 
-        public string Text { get; set; }
-        public int Value { get; set; }
+        public int Value { get; }
 
-        /// <summary>
-        /// Is this a single bit flag?
-        /// </summary>
-        public bool IsFlag => (Value & (Value - 1)) == 0;
-
-        /// <summary>
-        /// Is this part of a flag set?
-        /// </summary>
-        /// <param name="flags">The flag set.</param>
-        /// <returns>True if part of the flag set, otherwise false.</returns>
-        public bool IsMemberFlag(JmkFlagsCheckedListBoxItem flags) => IsFlag && (Value & flags?.Value) == Value;
+        private string Text { get; }
 
         public override string ToString() => Text;
     }
