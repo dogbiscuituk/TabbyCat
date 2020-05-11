5c725aa8caea49e7f8175e6b893273404d9dd15a
John Kerr <john@jmkerr.com>
2020-05-11 07:29:17 +0100

    Update RenderCon.cs

--- TabbyCat/Controllers/RenderCon.cs
+++ TabbyCat/Controllers/RenderCon.cs
@@ -1,78 +1,71 @@
 ﻿namespace TabbyCat.Controllers
 {
-    using Models;
     using OpenTK;
     using OpenTK.Graphics;
     using OpenTK.Graphics.OpenGL;
     using Properties;
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Globalization;
     using System.Linq;
     using System.Text;
     using Types;
     using Utils;
+    using Trace = Models.Trace;
 
     public partial class RenderCon : LocalizationCon
     {
         // Constructors
 
-        public RenderCon(WorldCon worldCon) : base(worldCon) => Stopwatch.Start();
-
-        // Public fields
-
-        public float FramesPerSecond { get; set; }
-
-        public bool SceneControlSuspended { get; set; }
-
-        public static GLInfo TheGLInfo { get; set; }
-
-        public static GraphicsMode TheGraphicsMode { get; set; }
+        public RenderCon(WorldCon worldCon) : base(worldCon) => _stopwatch.Start();
 
         // Private fields
 
         private bool
-            CameraViewValid,
-            ProgramCompiled,
-            ProjectionValid;
+            _cameraViewValid,
+            _programCompiled,
+            _projectionValid;
 
         private int
-            TickCount,
-            TickIndex;
+            _tickCount,
+            _tickIndex;
 
         private int
-            ProgramID,
-            VertexShaderID,
-            TessControlShaderID,
-            TessEvaluationShaderID,
-            GeometryShaderID,
-            FragmentShaderID,
-            ComputeShaderID;
+            _programId,
+            _vertexShaderId,
+            _tessControlShaderId,
+            _tessEvaluationShaderId,
+            _geometryShaderId,
+            _fragmentShaderId,
+            _computeShaderId;
 
         private int
-            Loc_CameraView,
-            Loc_Projection,
-            Loc_TimeValue,
-            Loc_TraceNumber,
-            Loc_Transform;
+            _locCameraView,
+            _locProjection,
+            _locTimeValue,
+            _locTraceNumber,
+            _locTransform;
 
-        private readonly List<int> Loc_Signals = new List<int>();
+        private readonly List<int> _locSignals = new List<int>();
 
-        private readonly long[] Ticks = new long[64];
+        private int _currencyCount;
 
-        private StringBuilder GpuLog;
+        private StringBuilder _gpuLog;
 
-        private readonly System.Diagnostics.Stopwatch Stopwatch = new System.Diagnostics.Stopwatch();
+        private readonly Stopwatch _stopwatch = new Stopwatch();
 
-        private int CurrencyCount;
+        private readonly long[] _ticks = new long[64];
 
         // Private static fields
 
         private static readonly object
-            GLInfoSyncRoot = new object(),
-            GLModeSyncRoot = new object();
+            InfoSyncRoot = new object(),
+            ModeSyncRoot = new object();
 
         // Public properties
+
+        public float FramesPerSecond { get; private set; }
 
         public GLInfo GLInfo
         {
@@ -82,16 +75,12 @@
                 {
                     GLInfo info = null;
                     UsingGL(() => info = new GLInfo());
-                    lock (GLInfoSyncRoot)
+                    lock (InfoSyncRoot)
                         TheGLInfo = info;
                 }
                 return TheGLInfo;
             }
         }
-
-        public List<ShaderType> ShaderTypes { get; } = new List<ShaderType>();
-
-        // Protected public properties
 
         public override GraphicsMode GraphicsMode
         {
@@ -101,18 +90,39 @@
                 {
                     GraphicsMode mode = null;
                     UsingGL(() => mode = SceneControl.GraphicsMode);
-                    lock (GLModeSyncRoot)
+                    lock (ModeSyncRoot)
                         TheGraphicsMode = mode;
                 }
                 return TheGraphicsMode;
             }
         }
 
+        public bool SceneControlSuspended { get; set; }
+
+        // Public static properties
+
+        public static GLInfo TheGLInfo { get; private set; }
+
         // Private properties
 
-        private bool ProgramValid => ProgramCompiled && Scene.GPUStatus == GPUStatus.OK;
+        private bool ProgramValid => _programCompiled && Scene.GPUStatus == GPUStatus.OK;
+
+        private List<ShaderType> ShaderTypes { get; } = new List<ShaderType>();
+
+        // Private static properties
+
+        private static GraphicsMode TheGraphicsMode { get; set; }
 
         // Public methods
+
+        public override void Connect(bool connect)
+        {
+            base.Connect(connect);
+            if (connect)
+                WorldCon.PropertyEdit += WorldCon_PropertyEdit;
+            else
+                WorldCon.PropertyEdit -= WorldCon_PropertyEdit;
+        }
 
         public void Invalidate()
         {
@@ -122,39 +132,27 @@
             InvalidateProjection();
         }
 
-        public void InvalidateAllTraces() => Scene.Traces.ForEach(InvalidateTrace);
-
         public void InvalidateProgram()
         {
-            ProgramCompiled = false;
+            _programCompiled = false;
             InvalidateCameraView();
             InvalidateProjection();
             UsingGL(() =>
             {
                 DeleteShaders();
-                if (ProgramID != 0)
+                if (_programId != 0)
                 {
-                    GL.DeleteProgram(ProgramID);
-                    ProgramID = 0;
+                    GL.DeleteProgram(_programId);
+                    _programId = 0;
                 }
             });
         }
 
-        public void InvalidateProjection() => ProjectionValid = false;
-
-        public void InvalidateTrace(Trace trace)
-        {
-            if (trace?.Vao != null)
-                UsingGL(() =>
-                {
-                    trace.Vao.ReleaseBuffers();
-                    trace.Vao = null;
-                });
-        }
+        public void InvalidateProjection() => _projectionValid = false;
 
         public void Refresh()
         {
-            lock (GLModeSyncRoot)
+            lock (ModeSyncRoot)
                 TheGraphicsMode = null;
             InvalidateProgram();
             InvalidateAllTraces();
@@ -169,7 +167,7 @@
             ValidateProgram();
             if (ProgramValid)
             {
-                GL.UseProgram(ProgramID); // Start Shader
+                GL.UseProgram(_programId); // Start Shader
                 ValidateCameraView();
                 ValidateProjection();
                 LoadParameters();
@@ -196,49 +194,9 @@
 
         public void Unload() => UsingGL(InvalidateAllTraces);
 
-        public bool UsingGL(Action action)
-        {
-            if (action == null)
-                return false;
-            var ok = !SceneControlSuspended &&
-                SceneControl?.IsHandleCreated == true &&
-                SceneControl?.HasValidContext == true &&
-                SceneControl?.Visible == true;
-            if (ok)
-            {
-                if (++CurrencyCount == 1)
-                    SceneControl.MakeCurrent();
-                try
-                {
-                    action();
-                }
-                finally
-                {
-                    if (--CurrencyCount == 0)
-                        SceneControl.Context.MakeCurrent(null);
-                }
-            }
-            return ok;
-        }
-
-        // Protected public methods
-
-        public override void Connect(bool connect)
-        {
-            base.Connect(connect);
-            if (connect)
-            {
-                WorldCon.PropertyEdit += WorldCon_PropertyEdit;
-            }
-            else
-            {
-                WorldCon.PropertyEdit -= WorldCon_PropertyEdit;
-            }
-        }
-
         // Private methods
 
-        private void BindAttribute(int attributeIndex, string variableName) => GL.BindAttribLocation(ProgramID, attributeIndex, variableName);
+        private void BindAttribute(int attributeIndex, string variableName) => GL.BindAttribLocation(_programId, attributeIndex, variableName);
 
         private void BindAttributes() => BindAttribute(0, "position");
 
@@ -248,44 +206,44 @@
             if (string.IsNullOrWhiteSpace(script))
                 return 0;
             Log($"Compiling {shaderType.ShaderName()};");
-            var shaderID = GL.CreateShader(shaderType);
-            GL.ShaderSource(shaderID, script);
-            GL.CompileShader(shaderID);
-            GL.AttachShader(ProgramID, shaderID);
-            Log(GL.GetShaderInfoLog(shaderID));
+            var shaderId = GL.CreateShader(shaderType);
+            GL.ShaderSource(shaderId, script);
+            GL.CompileShader(shaderId);
+            GL.AttachShader(_programId, shaderId);
+            Log(GL.GetShaderInfoLog(shaderId));
             ShaderTypes.Add(shaderType);
-            return shaderID;
+            return shaderId;
         }
 
         private void CreateShaders()
         {
             ShaderTypes.Clear();
-            VertexShaderID = CreateShader(ShaderType.VertexShader);
-            TessControlShaderID = CreateShader(ShaderType.TessControlShader);
-            TessEvaluationShaderID = CreateShader(ShaderType.TessEvaluationShader);
-            GeometryShaderID = CreateShader(ShaderType.GeometryShader);
-            FragmentShaderID = CreateShader(ShaderType.FragmentShader);
-            ComputeShaderID = CreateShader(ShaderType.ComputeShader);
+            _vertexShaderId = CreateShader(ShaderType.VertexShader);
+            _tessControlShaderId = CreateShader(ShaderType.TessControlShader);
+            _tessEvaluationShaderId = CreateShader(ShaderType.TessEvaluationShader);
+            _geometryShaderId = CreateShader(ShaderType.GeometryShader);
+            _fragmentShaderId = CreateShader(ShaderType.FragmentShader);
+            _computeShaderId = CreateShader(ShaderType.ComputeShader);
         }
 
-        private void DeleteShader(ref int shaderID)
+        private void DeleteShader(ref int shaderId)
         {
-            if (shaderID != 0)
+            if (shaderId != 0)
             {
-                GL.DetachShader(ProgramID, shaderID);
-                GL.DeleteShader(shaderID);
-                shaderID = 0;
+                GL.DetachShader(_programId, shaderId);
+                GL.DeleteShader(shaderId);
+                shaderId = 0;
             }
         }
 
         private void DeleteShaders()
         {
-            DeleteShader(ref VertexShaderID);
-            DeleteShader(ref TessControlShaderID);
-            DeleteShader(ref TessEvaluationShaderID);
-            DeleteShader(ref GeometryShaderID);
-            DeleteShader(ref FragmentShaderID);
-            DeleteShader(ref ComputeShaderID);
+            DeleteShader(ref _vertexShaderId);
+            DeleteShader(ref _tessControlShaderId);
+            DeleteShader(ref _tessEvaluationShaderId);
+            DeleteShader(ref _geometryShaderId);
+            DeleteShader(ref _fragmentShaderId);
+            DeleteShader(ref _computeShaderId);
         }
 
         private string GetSignalScript()
@@ -298,46 +256,58 @@
             return script.ToString();
         }
 
-        private int GetUniformLocation(string uniformName) => GL.GetUniformLocation(ProgramID, uniformName);
+        private int GetUniformLocation(string uniformName) => GL.GetUniformLocation(_programId, uniformName);
 
         private void GetUniformLocations()
         {
-            Loc_CameraView = GetUniformLocation("cameraView");
-            Loc_Projection = GetUniformLocation("projection");
-            Loc_TimeValue = GetUniformLocation("timeValue");
-            Loc_TraceNumber = GetUniformLocation("traceNumber");
-            Loc_Transform = GetUniformLocation("transform");
-            Loc_Signals.Clear();
+            _locCameraView = GetUniformLocation("cameraView");
+            _locProjection = GetUniformLocation("projection");
+            _locTimeValue = GetUniformLocation("timeValue");
+            _locTraceNumber = GetUniformLocation("traceNumber");
+            _locTransform = GetUniformLocation("transform");
+            _locSignals.Clear();
             foreach (var signal in Scene.Signals)
-                Loc_Signals.Add(GetUniformLocation(signal.Name));
+                _locSignals.Add(GetUniformLocation(signal.Name));
         }
 
-        private void InvalidateCameraView() => CameraViewValid = false;
+        private void InvalidateAllTraces() => Scene.Traces.ForEach(InvalidateTrace);
 
-        private void LoadCameraView() => LoadMatrix(Loc_CameraView, Scene.GetCameraView());
+        private void InvalidateCameraView() => _cameraViewValid = false;
+
+        private void InvalidateTrace(Trace trace)
+        {
+            if (trace?.Vao != null)
+                UsingGL(() =>
+                {
+                    trace.Vao.ReleaseBuffers();
+                    trace.Vao = null;
+                });
+        }
+
+        private void LoadCameraView() => LoadMatrix(_locCameraView, Scene.GetCameraView());
 
         private void LoadParameters() // Time and AC/DC signals.
         {
             var time = Clock.VirtualSecondsElapsed;
-            LoadFloat(Loc_TimeValue, time);
+            LoadFloat(_locTimeValue, time);
             for (var index = 0; index < Scene.Signals.Count; index++)
             {
                 var signal = Scene.Signals[index];
-                LoadFloat(Loc_Signals[index], signal.GetValueAt(time));
+                LoadFloat(_locSignals[index], signal.GetValueAt(time));
             }
         }
 
-        private void LoadProjection() => LoadMatrix(Loc_Projection, Scene.GetProjectionMatrix());
+        private void LoadProjection() => LoadMatrix(_locProjection, Scene.GetProjectionMatrix());
 
-        private void LoadTraceNumber(int traceIndex) => LoadInt(Loc_TraceNumber, traceIndex);
+        private void LoadTraceNumber(int traceIndex) => LoadInt(_locTraceNumber, traceIndex);
 
-        private void LoadTransform(Trace trace) => LoadMatrix(Loc_Transform, trace.GetTransform());
+        private void LoadTransform(Trace trace) => LoadMatrix(_locTransform, trace.GetTransform());
 
         private void Log(string s)
         {
             if (string.IsNullOrWhiteSpace(s))
                 return;
-            GpuLog.AppendLine(s.Trim());
+            _gpuLog.AppendLine(s.Trim());
             s = s.ToUpper(CultureInfo.InvariantCulture);
             if (s.Contains("ERROR"))
                 Scene.GPUStatus = GPUStatus.Error;
@@ -347,33 +317,57 @@
 
         private void UpdateFPS()
         {
-            Ticks[TickIndex = (TickIndex + 1) % Ticks.Length] = Stopwatch.ElapsedMilliseconds;
-            if (TickCount < Ticks.Length)
-                TickCount++;
+            _ticks[_tickIndex = (_tickIndex + 1) % _ticks.Length] = _stopwatch.ElapsedMilliseconds;
+            if (_tickCount < _ticks.Length)
+                _tickCount++;
             var fps = 0f;
-            if (TickCount > 1)
+            if (_tickCount > 1)
             {
-                var ticks = Ticks.Take(TickCount);
-                fps = 1000f * (TickCount - 1) / (ticks.Max() - ticks.Min());
+                var ticks = _ticks.Take(_tickCount).ToArray();
+                fps = 1000f * (_tickCount - 1) / (ticks.Max() - ticks.Min());
             }
             FramesPerSecond = fps;
         }
 
+        private void UsingGL(Action action)
+        {
+            if (action == null)
+                return;
+            var ok = !SceneControlSuspended &&
+                     SceneControl?.IsHandleCreated == true &&
+                     SceneControl?.HasValidContext == true &&
+                     SceneControl?.Visible == true;
+            if (ok)
+            {
+                if (++_currencyCount == 1)
+                    SceneControl.MakeCurrent();
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    if (--_currencyCount == 0)
+                        SceneControl.Context.MakeCurrent(null);
+                }
+            }
+        }
+
         private void ValidateCameraView()
         {
-            if (CameraViewValid)
+            if (_cameraViewValid)
                 return;
             LoadCameraView();
-            CameraViewValid = true;
+            _cameraViewValid = true;
         }
 
         private void ValidateProgram()
         {
-            if (ProgramCompiled)
+            if (_programCompiled)
                 return;
             Scene.GPUStatus = GPUStatus.OK;
-            GpuLog = new StringBuilder();
-            ProgramID = GL.CreateProgram();
+            _gpuLog = new StringBuilder();
+            _programId = GL.CreateProgram();
             CreateShaders();
             if (!ShaderTypes.Any())
                 Log("No Trace Shaders found to compile.");
@@ -381,25 +375,25 @@
             {
                 Log("Linking program;");
                 BindAttributes();
-                GL.LinkProgram(ProgramID);
-                GL.ValidateProgram(ProgramID);
-                Log(GL.GetProgramInfoLog(ProgramID));
+                GL.LinkProgram(_programId);
+                GL.ValidateProgram(_programId);
+                Log(GL.GetProgramInfoLog(_programId));
             }
             DeleteShaders();
             Log("End of log.");
-            Scene.GPULog = GpuLog.ToString().TrimEnd();
-            GpuLog = null;
+            Scene.GPULog = _gpuLog.ToString().TrimEnd();
+            _gpuLog = null;
             GetUniformLocations();
-            ProgramCompiled = true;
+            _programCompiled = true;
         }
 
         private void ValidateProjection()
         {
-            if (ProjectionValid)
+            if (_projectionValid)
                 return;
             GL.Viewport(SceneControl.Size);
             LoadProjection();
-            ProjectionValid = true;
+            _projectionValid = true;
         }
 
         private void ValidateTrace(Trace trace)
@@ -450,7 +444,7 @@
                 : Scene.Traces
                     .Select(p => string.Format(CultureInfo.InvariantCulture, Resources.Format_Case, p.Index + 1))
                     .Aggregate((p, q) => $"{p}{q}");
-            return !Scene.Traces.Any(p => !string.IsNullOrWhiteSpace(p.GetScript(shaderType)))
+            return Scene.Traces.All(p => string.IsNullOrWhiteSpace(p.GetScript(shaderType)))
                 ? string.Empty
                 : string.Format(
                     CultureInfo.InvariantCulture,
@@ -464,6 +458,6 @@
                     cases);
         }
 
-        public void SetScript(ShaderType shaderType, string value) => throw new System.NotImplementedException();
+        public void SetScript(ShaderType shaderType, string value) => throw new NotImplementedException();
     }
 }
