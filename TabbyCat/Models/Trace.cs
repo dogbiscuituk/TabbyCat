1fc47cfe45f243ed76e52fa866ce7190e6cd7382
John Kerr <john@jmkerr.com>
2020-05-11 12:37:06 +0100

    Update Trace.cs

--- TabbyCat/Models/Trace.cs
+++ TabbyCat/Models/Trace.cs
@@ -13,25 +13,25 @@
     {
         // Constructors
 
-        public Trace() : base() => Init();
+        public Trace() => Init();
 
         public Trace(Scene scene) : this() => Scene = scene;
 
-        public Trace(Trace trace) : base(trace) => CopyFrom(trace ?? null);
-
         // Private fields
 
-        private int _Index;
+        private int _index;
 
         // Public properties
 
         [DefaultValue("")]
         public string Description { get; set; }
 
-        public Pattern Pattern { get; set; }
-
-        [DefaultValue(true)]
-        public bool Visible { get; set; } = true;
+        [JsonIgnore]
+        public int Index
+        {
+            get => Scene?.Traces.IndexOf(this) ?? _index;
+            private set => _index = value;
+        }
 
         [JsonConverter(typeof(Vector3Converter))]
         public Vector3 Location { get; set; }
@@ -45,18 +45,10 @@
         [JsonConverter(typeof(Vector3Converter))]
         public Vector3 Orientation { get; set; }
 
+        public Pattern Pattern { get; set; }
+
         [JsonConverter(typeof(Vector3Converter))]
         public Vector3 Scale { get; set; }
-
-        [JsonConverter(typeof(Vector3Converter))]
-        public Vector3 StripeCount { get; set; }
-
-        [JsonIgnore]
-        public int Index
-        {
-            get => Scene?.Traces.IndexOf(this) ?? _Index;
-            private set => _Index = value;
-        }
 
         /// <summary>
         /// The Scene object which owns this Trace.
@@ -64,13 +56,21 @@
         [JsonIgnore]
         public Scene Scene { get; set; }
 
+        [JsonConverter(typeof(Vector3Converter))]
+        public Vector3 StripeCount { get; set; }
+
         /// <summary>
         /// The Video Array Object associated with this Trace.
         /// </summary>
         [JsonIgnore]
         public Vao Vao { get; set; }
 
+        [DefaultValue(true)]
+        public bool Visible { get; set; } = true;
+
         // Public methods
+
+        public Matrix4 GetTransform() => MathUtils.CreateTransformation(Location, Orientation, Scale);
 
         public override string ToString() => !string.IsNullOrWhiteSpace(Description)
             ? Description
@@ -78,48 +78,11 @@
             ? $"Trace #{Index + 1}"
             : "New trace";
 
-        public Matrix4 GetTransform() => MathUtils.CreateTransformation(Location, Orientation, Scale);
-
-        public string PreviewShader(ShaderType shaderType, string formula)
-        {
-            var script = GetScript(shaderType);
-            var beginLine = script.FindFirstTokenLine(Tokens.BeginFormula) + 1;
-            var endLine = script.FindFirstTokenLine(Tokens.EndFormula);
-            if (0 <= beginLine && beginLine < endLine)
-            {
-                string
-                    head = script.GetLines(0, beginLine),
-                    tail = script.GetLines(endLine, script.GetLineCount() - endLine);
-                return $"{head}\r\n\r\n{formula}\r\n\r\n{tail}";
-            }
-            return string.Empty;
-        }
-
-        public void SetLocation(Vector3 location) => Location = location;
-
-        public void SetOrientation(Vector3 orientation) => Orientation = orientation;
-
-        public void SetScale(Vector3 scale) => Scale = scale;
-
         // Protected methods
 
         protected void SetFormula(ShaderType shaderType, string formula) => SetScript(shaderType, PreviewShader(shaderType, formula));
 
         // Private methods
-
-        private void CopyFrom(Trace trace)
-        {
-            Description = trace.Description;
-            Index = trace.Index;
-            Location = new Vector3(trace.Location);
-            Maximum = new Vector3(trace.Maximum);
-            Minimum = new Vector3(trace.Minimum);
-            Orientation = new Vector3(trace.Orientation);
-            Pattern = trace.Pattern;
-            Scale = new Vector3(trace.Scale);
-            StripeCount = new Vector3(trace.StripeCount);
-            Visible = trace.Visible;
-        }
 
         private void Init()
         {
@@ -144,6 +107,21 @@
             GeometryShader = Resources.Trace_GeometryShader;
             FragmentShader = Resources.Trace_FragmentShader;
             ComputeShader = Resources.Trace_ComputeShader;
+        }
+
+        private string PreviewShader(ShaderType shaderType, string formula)
+        {
+            var script = GetScript(shaderType);
+            var beginLine = script.FindFirstTokenLine(Tokens.BeginFormula) + 1;
+            var endLine = script.FindFirstTokenLine(Tokens.EndFormula);
+            if (0 <= beginLine && beginLine < endLine)
+            {
+                string
+                    head = script.GetLines(0, beginLine),
+                    tail = script.GetLines(endLine, script.GetLineCount() - endLine);
+                return $"{head}\r\n\r\n{formula}\r\n\r\n{tail}";
+            }
+            return string.Empty;
         }
     }
 }
