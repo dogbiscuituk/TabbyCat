d17c9640a5b1c4982799e4ce9de2bde611e5eeb8
John Kerr <john@jmkerr.com>
2020-05-11 08:49:01 +0100

    Update ShaderCodeCon.cs

--- TabbyCat/Controllers/ShaderCodeCon.cs
+++ TabbyCat/Controllers/ShaderCodeCon.cs
@@ -11,28 +11,36 @@
 
     public class ShaderCodeCon : CodeCon
     {
+        // Constructors
+
         public ShaderCodeCon(WorldCon worldCon) : base(worldCon) { }
 
-        private readonly List<int> Breaks = new List<int>();
+        // Private fields
+
+        private readonly List<int> _breaks = new List<int>();
+
+        // Protected properties
 
         protected override Property Shader => ShaderType.ShaderProperty();
 
         protected override IScript ShaderSet => RenderCon;
 
-        protected override string GetRegion() => Resources.ShaderRegion_All;
+        // Public methods
 
         public override void Connect(bool connect)
         {
             base.Connect(connect);
             if (connect)
-            {
                 WorldForm.ViewShaderCode.Click += ViewShaderCode_Click;
-            }
             else
-            {
                 WorldForm.ViewShaderCode.Click -= ViewShaderCode_Click;
-            }
         }
+
+        // Protected methods
+
+        protected override bool CodeChanged(Property property) => property.InvalidatesProgram();
+
+        protected override string GetRegion() => Resources.ShaderRegion_All;
 
         protected override void LoadScript()
         {
@@ -49,42 +57,42 @@
         protected override void RunShaderCommand(string text)
         {
             FindBreaks(text);
-            if (Breaks.Any())
+            if (!_breaks.Any())
+                return;
+            for (var traceNumber = 0; traceNumber <= Scene.Traces.Count; traceNumber++)
             {
-                for (var traceNumber = 0; traceNumber <= Scene.Traces.Count; traceNumber++)
-                {
-                    string
-                        oldScript = traceNumber == 0 ? Scene.GetScript(ShaderType) : Scene.Traces[traceNumber - 1].GetScript(ShaderType),
-                        newScript = ExtractScript(text, traceNumber).Outdent("  ");
-                    if (newScript != oldScript)
-                        if (traceNumber == 0)
-                            Run(new SceneShaderCommand(ShaderType, newScript));
-                        else
-                            Run(new TraceShaderCommand(traceNumber - 1, ShaderType, newScript));
-                }
+                string
+                    oldScript = traceNumber == 0 ? Scene.GetScript(ShaderType) : Scene.Traces[traceNumber - 1].GetScript(ShaderType),
+                    newScript = ExtractScript(text, traceNumber).Outdent("  ");
+                if (newScript == oldScript)
+                    continue;
+                if (traceNumber == 0)
+                    Run(new SceneShaderCommand(ShaderType, newScript));
+                else
+                    Run(new TraceShaderCommand(traceNumber - 1, ShaderType, newScript));
             }
         }
 
-        protected override bool CodeChanged(Property property) => property.InvalidatesProgram();
+        // Private methods
 
         private bool AddBreak(int b)
         {
-            var ok = !Breaks.Any() || b > Breaks.Last();
+            var ok = !_breaks.Any() || b > _breaks.Last();
             if (ok)
-                Breaks.Add(b);
+                _breaks.Add(b);
             return ok;
         }
 
         private string ExtractScript(string text, int traceNumber)
         {
             var breakIndex = 2 * traceNumber + 1;
-            int start = Breaks[breakIndex], end = Breaks[breakIndex + 1];
+            int start = _breaks[breakIndex], end = _breaks[breakIndex + 1];
             return text.GetLines(start, end - start);
         }
 
         private void FindBreaks(string script)
         {
-            Breaks.Clear();
+            _breaks.Clear();
             if (string.IsNullOrWhiteSpace(script))
                 return;
             var ok = AddBreak(0) && AddBreak(script.FindFirstTokenLine(Tokens.BeginScene) + 2) && AddBreak(script.FindFirstTokenLine(Tokens.EndScene) - 1);
@@ -92,9 +100,9 @@
                 ok &= AddBreak(script.FindFirstTokenLine(Tokens.BeginTrace(index)) + 2) & AddBreak(script.FindFirstTokenLine(Tokens.EndTrace(index)) - 1);
             ok &= AddBreak(script.GetLineCount());
             if (!ok)
-                Breaks.Clear();
-            for (var index = 0; index < Breaks.Count; index += 2)
-                PrimaryCon.AddSystemRange(new Range(PrimaryTextBox, 0, Breaks[index], 0, Breaks[index + 1]));
+                _breaks.Clear();
+            for (var index = 0; index < _breaks.Count; index += 2)
+                PrimaryCon.AddSystemRange(new Range(PrimaryTextBox, 0, _breaks[index], 0, _breaks[index + 1]));
         }
 
         private void ViewShaderCode_Click(object sender, EventArgs e) => ToggleVisibility();
