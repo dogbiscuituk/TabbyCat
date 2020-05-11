250d856a547130da4e36ab5eb0c0fc63f73a3304
John Kerr <john@jmkerr.com>
2020-05-11 07:33:02 +0100

    Update SceneCodeCon.cs

--- TabbyCat/Controllers/SceneCodeCon.cs
+++ TabbyCat/Controllers/SceneCodeCon.cs
@@ -8,7 +8,11 @@
 
     public class SceneCodeCon : CodeCon
     {
+        // Constructors
+
         public SceneCodeCon(WorldCon worldCon) : base(worldCon) { }
+
+        // Protected properties
 
         protected override Property Shader => ShaderType.SceneShader();
 
@@ -16,18 +20,18 @@
 
         protected override string GetRegion() => Resources.ShaderRegion_Scene;
 
+        // Public methods
+
         public override void Connect(bool connect)
         {
             base.Connect(connect);
             if (connect)
-            {
                 WorldForm.ViewSceneCode.Click += ViewSceneCode_Click;
-            }
             else
-            {
                 WorldForm.ViewSceneCode.Click -= ViewSceneCode_Click;
-            }
         }
+
+        // Protected methods
 
         protected override void Localize()
         {
@@ -36,6 +40,8 @@
         }
 
         protected override void RunShaderCommand(string text) => Run(new SceneShaderCommand(ShaderType, text));
+
+        // Private methods
 
         private void ViewSceneCode_Click(object sender, EventArgs e) => ToggleVisibility();
     }
